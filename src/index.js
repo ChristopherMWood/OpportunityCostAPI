3a1e4bfc93dbee582b637d25abd1d4f632a25d43
Christopher Wood <cmw2379@gmail.com>
2022-05-24 18:25:22 -0400

    Added cors support

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import express from 'express';
+import cors from 'cors';
+
 import dotenv from 'dotenv';
 import { YoutubeApiProxy } from './youtubeApiProxy.js';
 import { ParsingHelpers } from './parsingHelpers.js';
 
-const app = express();
 dotenv.config();
 
 if (!process.env.PORT || !process.env.GOOGLE_API_KEY) {
     throw new Error('ERROR: PORT or GOOGLE_API_KEY environment variable has not been correctly set!');
 }
+
+const app = express();
+
+app.use((req, res, next) => {
+    //res.header('Access-Control-Allow-Origin', '*'); // allow cors for this endpoint... VERIFY
+    next();
+})
+
+app.use(cors());
 
 app.get('/api/opportunityCost/:youtubeVideoId', (req, res) => {
     res.type('application/json'); 
@@ -26,7 +36,7 @@ app.get('/api/opportunityCost/:youtubeVideoId', (req, res) => {
             formattedTime: ParsingHelpers.getTimeFromTotalSeconds(totalSecondsOfViews)
         }));
     }, function(error) {
-        console.log(error);
+        //console.log(error);
         res.status(400);
         res.send(JSON.stringify({
             message: 'An unknown error occured loading the data'
