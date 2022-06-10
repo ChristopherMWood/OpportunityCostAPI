dda00b097baa26ddd0b5b999c467fc08b40899ca
Christopher Wood <cmw2379@gmail.com>
2022-06-10 19:44:30 -0400

    Corrected static path

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ if (!process.env.PORT || !process.env.GOOGLE_API_KEY) {
 }
 
 const app = express();
-app.use(express.static(path.join(__dirname, '../static_files')));
+app.use(express.static(path.join(__dirname, '../../static_files')));
 
 // app.use((req, res, next) => {
 //     //res.header('Access-Control-Allow-Origin', '*'); // allow cors for this endpoint... VERIFY
