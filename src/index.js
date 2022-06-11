3c2fa10f4b0832b7423f8f34df549c3c35947254
Christopher Wood <cmw2379@gmail.com>
2022-06-10 22:14:33 -0400

    CD pipeline test

--- src/index.js
+++ src/index.js
@@ -31,5 +31,5 @@ app.get('*', (req, res) => {
 let server = app;
 
 server.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+    console.log(`Server running on port ${process.env.PORT}...`);
 });
