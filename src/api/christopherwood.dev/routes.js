e5cc600e36ceddc2ba3ab3eb320a3825270e0f5b
Christopher Wood <cmw2379@gmail.com>
2022-07-16 13:28:27 -0400

    removing front-end hosting

--- src/api/christopherwood.dev/routes.js
+++ src/api/christopherwood.dev/routes.js
@@ -1,12 +1,16 @@
 import express from 'express'
+import sendEmail from '../../services/emailService.js'
 
 const router = express.Router()
 
-router.post('/contact', (req, res) => {
-	res.status(200)
-	res.send(JSON.stringify({
-		message: 'Not implemented yet'
-	}))
+router.post('/contact', async (req, res) => {
+	sendEmail(req.body, process.env.CONTACT_EMAIL, () => {
+		res.status(200)
+		res.send()
+	}, () => {
+		res.status(400)
+		res.send()
+	})
 })
 
 router.get('/health-check', (req, res) => {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import path from 'path'
-import { fileURLToPath } from 'url'
 import apiRoutes from './api/routes.js'
 import logger from './logger.js'
 
@@ -11,12 +10,9 @@ dotenv.config()
 if (!process.env.PORT || !process.env.GOOGLE_API_KEY) {
 	throw new Error('ERROR: PORT or GOOGLE_API_KEY environment variable has not been correctly set!')
 }
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 const app = express()
-app.use(express.static(path.join(__dirname, '../../static_files')))
 app.use(cors())
+app.use(bodyParser.json())
 
 const apiRouteRequestLogging = (req, res, next) => {
 	const requestedEndpoint = req.protocol + '://' + req.get('Host') + req.url
@@ -31,13 +27,6 @@ app.use((err, req, res, next) => {
 	logger.error(err)
 	res.status(500).send('An unknown error occured')
 	next() //I think this is needed <-------check
-})
-
-/* Static File Server for frontend */
-app.get('*', (req, res) => {
-	const requestedEndpoint = req.protocol + '://' + req.get('Host') + req.url
-	logger.info(`STATIC File Request: ${requestedEndpoint}`)
-	res.sendFile(path.join(__dirname, '../../static_files/index.html'))
 })
 
 app.listen(process.env.PORT, () => {
